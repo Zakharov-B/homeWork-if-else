464cb5056517b62a598adb149929d619f40b5e5e
boriszakharov <zakharovboriska@mail.ru>
2026-09-24 03:16:45 +0700

    Complete if-else homework

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,8 @@ public class Main {
             System.out.println("Если возраст ребенка равен " + childAge + ", то ему нельзя кататься на аттракционе");
         } else if (childAge >= 5 && childAge <= 14) {
             System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        } else {
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататься на аттракционе без сопровождения взрослого");
         }
         int passengers = 85;
         int totalSeats = 102;
